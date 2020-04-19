f48807ea0297a910c376cbca15f612bbddf39df1
Marc Strong <marcstrong@yahoo.com>
2020-04-19 15:03:55 -0500

    index.html, add link for href localhost link to notes.html

--- server.js
+++ server.js
@@ -6,38 +6,54 @@ const PORT = 5150;
 var server = http.createServer(handleRequest);
 
 server.listen(PORT, function(){
-    console.log("Server listening on: http://localhost:" + PORT + "/notes");
+    console.log("Server listening on: http://localhost:" + PORT + "/");
 });
 
-function handleRequest(request, response, PORT) {
+function handleRequest(request, response) {
 
     // Capture the url the request is made to
     var path = request.url;
   
     // Depending on the URL, display a different HTML file.
     switch (path) {
-  
-    case "/notes":
-      return displayNotes(response, PORT);
-  
-    
+
+      case "/notes":
+        return displayNotes(response);
+      default:
+        return displayIndex(response);   
     }
-  }
+}
 
-  function displayNotes(response, PORT) {
-    filename = "./public/index.html";
-    fs.readFile(filename, "utf8", function(error, data) {
-        if (error) {
-          return console.log(error);
-        }
+function displayIndex(response) {
+  filename = "./public/index.html";
+  fs.readFile(filename, "utf8", function(error, data) {
+      if (error) {
+        return console.log(error);
+      }
 
-        console.log(data) 
+      console.log(data) 
 
+  // Configure the response to return a status code of 200 (meaning everything went OK), and to be an HTML document
+  response.writeHead(200, { "Content-Type": "text/html" });
 
-    // Configure the response to return a status code of 200 (meaning everything went OK), and to be an HTML document
-    response.writeHead(200, { "Content-Type": "text/html" });
-  
-    // End the response by sending the client the myHTML string (which gets rendered as an HTML document thanks to the code above)
-    response.end(data);
-});
-  }
+  // End the response by sending the client the myHTML string (which gets rendered as an HTML document thanks to the code above)
+  response.end(data);
+  });
+}
+
+function displayNotes(response) {
+  filename = "./public/notes.html";
+  fs.readFile(filename, "utf8", function(error, data) {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(data) 
+
+  // Configure the response to return a status code of 200 (meaning everything went OK), and to be an HTML document
+  response.writeHead(200, { "Content-Type": "text/html" });
+
+  // End the response by sending the client the myHTML string (which gets rendered as an HTML document thanks to the code above)
+  response.end(data);
+  });
+}
